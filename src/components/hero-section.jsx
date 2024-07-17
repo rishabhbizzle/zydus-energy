c73f8029c7e2996f32268ce7425199863e35970c
rishabhbizzle <rishabhbizzle@gmail.com>
2024-07-17 23:43:31 +0530

    feat: carousel added

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,12 +1,20 @@
 "use client";
 
 import React from "react";
-import { ArrowBigDownDash, CheckIcon, FileText } from "lucide-react";
 import { AnimatePresence, motion, useInView } from "framer-motion";
 import { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const HeroSection = () => {
   const FADE_DOWN_ANIMATION_VARIANTS = {
@@ -16,6 +24,9 @@ const HeroSection = () => {
 
   const heroSectionRef = useRef(null);
   const heroSectionInView = useInView(heroSectionRef);
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
 
   return (
     <div className="bg-gray-900 text-white flex min-h-[90vh] p-8 justify-center items-center">
@@ -40,20 +51,14 @@ const HeroSection = () => {
               variants={FADE_DOWN_ANIMATION_VARIANTS}
               className="space-x-4 text-3xl font-bold tracking-tight sm:text-5xl"
             >
-              Zydus Green Energy pvt ltd
+              Zydus Green Energy Pvt Ltd
             </motion.h1>
-            {/* <motion.p
-              variants={FADE_DOWN_ANIMATION_VARIANTS}
-              className="space-x-4 text-xl font-bold tracking-tight sm:text-3xl mt-3"
-            >
-              Your ultimate music analytics platform
-            </motion.p> */}
 
             <motion.p
               variants={FADE_DOWN_ANIMATION_VARIANTS}
               className="mt-6 text-base sm:text-lg leading-8 "
             >
-              Zydus Green Energy PVT LTD is a leading provider of renewable
+              Zydus Green Energy Pvt Ltd is a leading provider of renewable
               energy solutions, committed to driving the transition to a
               greener, more sustainable world.
             </motion.p>
@@ -73,18 +78,36 @@ const HeroSection = () => {
             </motion.div>
           </motion.div>
         </div>
-        <div className="flex">
-          <img
-            alt="Zydus Green Energy"
-            className="mt-8 md:mt-0 max-w-[600px] w-full"
-            height="400"
-            src="https://i0.wp.com/indianinfrastructure.com/wp-content/uploads/2022/11/II-24.jpg?fit=1855%2C1042&ssl=1"
-            style={{
-              aspectRatio: "600/400",
-              objectFit: "cover",
-            }}
-            width="600"
-          />
+        <div className="w-1/2 hidden md:flex">
+          <Carousel
+            plugins={[
+              Autoplay({
+                delay: 3500,
+              }),
+            ]}
+            className="w-full"
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={plugin.current.reset}
+          >
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <img
+                    alt="Zydus Green Energy"
+                    className="mt-8 md:mt-0 max-w-[600px] w-full"
+                    height="400"
+                    src="https://i0.wp.com/indianinfrastructure.com/wp-content/uploads/2022/11/II-24.jpg?fit=1855%2C1042&ssl=1"
+                    style={{
+                      aspectRatio: "600/400",
+                      objectFit: "cover",
+                    }}
+                    width="600"
+                    loading="lazy"
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </div>
       </div>
     </div>
